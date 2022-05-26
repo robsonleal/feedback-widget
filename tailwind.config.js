b4982994a6e565b7d1c5116b39de642dde229a4f
Robson Leal <devleal@hotmail.com>
2022-05-26 16:03:03 -0300

    Finalizando aula 01

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          500: '#8257e6',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
