dab23c9abeea6da01bf5149dd6f4c569d926ee03
Robson Leal <devleal@hotmail.com>
2022-05-30 17:48:56 -0300

    Criando testes automatizados e configurando o CORS da aplicacao

--- server/src/routers.ts
+++ server/src/routers.ts
@@ -2,9 +2,8 @@ import express from 'express';
 import { SubmitFeedbackUseCase } from './use-cases/submit-feedback-use-case';
 import { PrismaFeedbackRepository } from './repositories/prisma/prisma-feedbacks-repository';
 import { NodemailerMailAdapter } from './adapters/nodemailer/nodemailer-mail-adapter';
+
 export const routes = express.Router();
-
-
 
 routes.post('/feedbacks', async (req, res) => {
     const { type, comment, screenshot } = req.body
